11bd64e43d724acb36b67cd20300effc52c9ca16
lsd7nk <87329383+s-elovikov@users.noreply.github.com>
2026-02-13 15:30:00 +0700

    added trace & critical logs

--- src/Runtime/Core/Interfaces/ILoggerService.cs
+++ src/Runtime/Core/Interfaces/ILoggerService.cs
@@ -8,19 +8,25 @@ namespace Nk7.Logger
     {
         bool IsEnabled(LogLevel logLevel);
 
+        void Trace(string message);
         void Debug(string message);
         void Information(string message);
         void Warning(string message);
         void Error(string message);
+        void Critical(string message);
 
+        void Trace([InterpolatedStringHandlerArgument("")] ref TraceLogInterpolatedStringHandler handler);
         void Debug([InterpolatedStringHandlerArgument("")] ref DebugLogInterpolatedStringHandler handler);
         void Information([InterpolatedStringHandlerArgument("")] ref InformationLogInterpolatedStringHandler handler);
         void Warning([InterpolatedStringHandlerArgument("")] ref WarningLogInterpolatedStringHandler handler);
         void Error([InterpolatedStringHandlerArgument("")] ref ErrorLogInterpolatedStringHandler handler);
+        void Critical([InterpolatedStringHandlerArgument("")] ref CriticalLogInterpolatedStringHandler handler);
 
+        void Trace(Exception exception, [InterpolatedStringHandlerArgument("")] ref TraceLogInterpolatedStringHandler handler);
         void Debug(Exception exception, [InterpolatedStringHandlerArgument("")] ref DebugLogInterpolatedStringHandler handler);
         void Information(Exception exception, [InterpolatedStringHandlerArgument("")] ref InformationLogInterpolatedStringHandler handler);
         void Warning(Exception exception, [InterpolatedStringHandlerArgument("")] ref WarningLogInterpolatedStringHandler handler);
         void Error(Exception exception, [InterpolatedStringHandlerArgument("")] ref ErrorLogInterpolatedStringHandler handler);
+        void Critical(Exception exception, [InterpolatedStringHandlerArgument("")] ref CriticalLogInterpolatedStringHandler handler);
     }
 }
